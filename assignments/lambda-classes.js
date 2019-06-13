9060e6702f4b0264ef3729f06f22303f6f7462b7
Kristin Barr <kristinbarr@gmail.com>
2019-06-13 18:39:01 -0400

    MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,125 @@
 // CODE here for your Lambda Classes
+
+class People {
+  constructor(attr) {
+    this.name = attr.name
+    this.location = attr.location
+    this.age = attr.age
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
+}
+
+class Instructor extends People {
+  constructor(attr) {
+    super(attr)
+    this.favLanguage = attr.favLanguage
+    this.specialty = attr.specialty
+    this.catchPhrase = attr.catchPhrase
+  }
+  demo(subject) { // subject is string
+    return `Today we are learning about ${subject}.`
+  }
+  grade(student, subject) { // student is object, subject is string
+    return `${student.name} receives a perfect sore on ${subject}`
+  }
+}
+
+class Student extends Person {
+  constructor(attr) {
+    super(attr)
+    this.previousBackground = attr.previousBackground
+    this.className = attr.className
+    this.favSubject = attr.favSubject
+  }
+  listsSubject() {
+    this.favSubject.map(subject => {
+      console.log(subject)
+    })
+  }
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`)
+  }
+  sprintChallenge(subject) {
+    console.log(`${this.name} has begun sprint challenge on ${subject}`)
+  }
+}
+
+class ProjectManager extends Instructor {
+  constructor(attr) {
+    super(attr)
+    this.gradClassName = attr.gradClassName
+    this.favInstructor = attr.favInstructor
+  }
+  standUp(channel) {
+    console.log(`${this.name} announces to ${channel}, @${channel} standby times!`)
+  }
+  debugsCode(student, subject) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
+  }
+}
+
+const becka = new Instructor({
+  name: 'Becka',
+  location: 'Houston',
+  age: 28,
+  favLanguage: 'Go',
+  specialty: 'Front-End',
+  catchPhrase: 'Ashes to ashes, dust to side chicks.'
+})
+
+const adrian = new Instructor({
+  name: 'Adrian',
+  location: 'Brooklyn',
+  age: 99,
+  favLanguage: 'Python',
+  specialty: 'Back-End',
+  catchPhrase: "Life is better in Pajamas"
+})
+
+const blondy = new Student({
+  name: 'Blondy',
+  location: 'Los Angeles',
+  age: 19,
+  favLanguage: 'JavaScript',
+  specialty: 'Fullstack',
+  catchPhrase: "I can't do this but I'm doing it anyway!",
+  previousBackground: "hand model",
+  className: "WEB21",
+  favSubject: ['CSS', 'HTML','JavaScript', 'React']
+})
+
+const ellis = new Student({
+  name: 'Ellis',
+  location: 'Denver',
+  age: 59,
+  favLanguage: 'Ruby',
+  specialty: 'Back-End',
+  catchPhrase: 'Turn down for what',
+  previousBackground: 'taxodermist',
+  className: 'WEB19',
+  favSubject: ['Ruby', 'SQL', 'APIs']
+})
+
+const joy = new ProjectManager({
+  name: 'Joy',
+  location: 'Miami',
+  age: 73,
+  favLanguage: 'JavaScript',
+  specialty: 'Front-End',
+  catchPhrase: "I'm doing whatever I want now",
+  gradClassName: 'WEB6',
+  favInstructor: 'Josh'
+})
+
+const terrance = new ProjectManager({
+  name: 'Terrance',
+  location: 'Chicago',
+  age: 47,
+  favLanguage: 'Node.js',
+  specialty: 'Back-End',
+  catchPhrase: "OMG Becky, look at her back-end",
+  gradClassName: 'CS1',
+  favInstructor: 'Dan'
+})
